3d2f383f68d7447e5c0cf1522c79d89a4c57bd1a
Zunzix <59012677+zunzix@users.noreply.github.com>
2024-04-17 15:16:54 +0300

    Add files via upload

---
+++ Webassembly/ver4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <emscripten.h>
+
+
+int main(){
+		printf("Hello world!");
+		return 0;
+}
+
+EMSCRIPTEN_KEEPALIVE
+float kuluvAeg(float vahemaa, float kiirus){
+
+	float aeg;
+	aeg = vahemaa / kiirus;
+	return aeg;
+}
